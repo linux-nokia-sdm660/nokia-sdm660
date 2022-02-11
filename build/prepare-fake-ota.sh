8ad79d41aa2d11bb245620cbc7b381a2b61d8720
Alberto Mardegan <mardy@users.sourceforge.net>
2022-02-12 00:15:47 +0300

    build/prepare-fake-ota.sh: use 20.04 channel for focal

--- build/prepare-fake-ota.sh
+++ build/prepare-fake-ota.sh
@@ -4,11 +4,12 @@
 URL='https://system-image.ubports.com'
 if [ "$TARGET_DISTRO" == "focal" ]; then
     ROOTFS_URL='https://ci.ubports.com/job/focal-hybris-rootfs-arm64/job/master/lastSuccessfulBuild/artifact/ubuntu-touch-android9plus-rootfs-arm64.tar.gz'
+    OTA_CHANNEL='20.04/arm64/android9/devel'
 else
     ROOTFS_URL='https://ci.ubports.com/job/xenial-hybris-android9-rootfs-arm64/lastSuccessfulBuild/artifact/ubuntu-touch-android9-arm64.tar.gz'
+    OTA_CHANNEL='16.04/arm64/android9/devel'
 fi
 DEVICE_GENERIC_URL='https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%252Fcommunity-ports%252Fjenkins-ci%252Fgeneric_arm64/job/main/lastSuccessfulBuild/artifact/halium_halium_arm64.tar.xz'
-OTA_CHANNEL='16.04/arm64/android9/devel'
 
 DEVICE_TARBALL="$1"
 OUTPUT="$2"
